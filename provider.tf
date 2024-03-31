37d25f6b4a41b355904dbfef619ad70e3d0bb0c8
Christian <crissbritez70@gmail.com>
2024-03-31 02:52:50 -0300

    CICD_TEST

--- provider.tf
+++ provider.tf
@@ -1,15 +1,14 @@
 terraform {
   required_providers {
     google = {
-        source  = "hashicorp/google"
-        version = "5.22.0"
+      source  = "hashicorp/google"
+      version = "5.22.0"
     }
   }
 }
 provider "google" {
-    project     = "cit262chris"
-    region      = "us-west1"
-    zone        = "us-west1-b"
-    credentials = "./cit262chris-1cbf4c3f63d5.json"
-  
+  project     = "cit262chris"
+  region      = "us-west1"
+  zone        = "us-west1-b"
+  credentials = "./cit262chris-1cbf4c3f63d5.json"
 }

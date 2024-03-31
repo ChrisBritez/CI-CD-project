36418f7811ae7516eb104f8a5fcbae24ddf58798
Christian <crissbritez70@gmail.com>
2024-03-31 02:11:35 -0300

    first-test-terraform

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+        source  = "hashicorp/google"
+        version = "5.22.0"
+    }
+  }
+}
+provider "google" {
+    project     = "cit262chris"
+    region      = "us-west1"
+    zone        = "us-west1-b"
+    credentials = "./cit262chris-1cbf4c3f63d5.json"
+  
+}
